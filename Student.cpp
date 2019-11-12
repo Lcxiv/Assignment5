7015cf1c9353599be100c84a44550e5868166597
Louis Condevaux <condevaux@chapman.edu>
2019-11-11 16:32:18 -0800

    added functions to the student class

--- Student.cpp
+++ Student.cpp
@@ -1,14 +1,43 @@
 #include<iostream>
 #include "Student.h"
-using namespace std;
+
 
 Student::Student(){
-  studentID = NULL;
-  nameField = NULL;
-  levelField = NULL;
-  majorField = NULL;
-  studentGPA = NULL;
-  advisorField = NULL;
+  studentID = -1;
+  nameField;
+  levelField;
+  majorField;
+  studentGPA = 0;
+  advisorField = -1;
 }
 
-Student::Student(int studID,string nameF, string levelF = "Freshman", string majorF = "Undecided", double GPAtotal, int advisorF): studentID(studID), nameField(nameF), levelField(levelF), majorField(majorF), studentGPA(GPAtotal), advisorField(advisorF){};
+Student::Student(int studID,std::string nameF, std::string levelF, std::string majorF, double GPAtotal, int advisorF):
+studentID(studID), nameField(nameF), levelField(levelF), majorField(majorF), studentGPA(GPAtotal), advisorField(advisorF){};
+
+
+//member functions
+
+bool Student::GreaterThan(int &comp1, int &comp2)
+{
+  if (comp1 > comp2)
+  {
+    std::cout<<"The student A is superior than student B"<<"\n";
+    return true;
+  }
+}
+bool Student::compareSTR(std::string &a, std::string &b)
+{
+
+  if (a == "Freshman" && b == "Freshman") return true;
+  else if (a == "Sophomore" && b == "Freshman") return true;
+  //finish it
+}
+void Student::print()
+{
+  std::cout<<"the Student's ID is: "<<getStudentID()<<"\n";
+  std::cout<<"the Student's advisor is: "<<getAdvisorField()<<"\n";
+  std::cout<<"the Student's GPA is: "<<getGPA()<<"\n";
+  std::cout<<"the Student's field is: "<<getNameField()<<"\n";
+  std::cout<<"the Student's level is: "<<getLevelField()<<"\n";
+  std::cout<<"the Student's major is: "<<getMajorField()<<"\n";
+}
--- Student.h
+++ Student.h
@@ -1,19 +1,27 @@
-using namespace std;
+#ifndef STUDENT_H
+#define STUDENT_H
+#include <iostream>
+
+
 class Student
 {
   int studentID, advisorField;
   double studentGPA;
-  string nameField, levelField, majorField;
+  std::string nameField, levelField, majorField;
 
 public:
   Student();
-  Student(int studID,string nameF, string levelF = "Freshman", string majorF = "Undecided", double GPAtotal, int advisorF);
-  ~Student;
+  Student(int studID,std::string nameF, std::string levelF, std::string majorF, double GPAtotal, int advisorF);
+  ~Student();
 
-  int getStudentID();
-  int getAdvisorField();
-  double getGPA();
-  string getNameField();
-  string getLevelField();
-  string getMajorField();
-}
+  int getStudentID(){return studentID;};
+  int getAdvisorField(){return advisorField;};
+  double getGPA(){return studentGPA;};
+  std::string getNameField(){return nameField;};
+  std::string getLevelField(){return levelField;};
+  std::string getMajorField(){return majorField;};
+  bool GreaterThan(int &comp1, int &comp2);
+  bool compareSTR(std::string &a, std::string &b);
+  void print();
+};
+#endif
